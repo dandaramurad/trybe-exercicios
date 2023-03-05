88c0e57b3960c7358b121cd4f352d79b62e7b807
Dandara Murad Cajaiba <dandymi@gmail.com>
2023-03-05 20:42:30 -0300

    conferindo o maior de dois números

---
+++ fundamentos/secao-3-introdjava/dia-1-primeirospassos/exercicio.js
@@ -0,0 +1,12 @@
+let numberA = 21;
+let numberB = 21;
+
+if (numberA > numberB) {
+    console.log('A é maior que B');
+}
+else if (numberB > numberA) {
+    console.log('B é maior que A');
+}
+else {
+    console.log('São iguais');
+}
