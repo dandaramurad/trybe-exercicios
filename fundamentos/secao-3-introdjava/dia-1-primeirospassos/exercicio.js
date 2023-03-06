956ad5bf4f2a4ebc1830e03ba065c84c48cf0f46
Dandara Murad Cajaiba <dandymi@gmail.com>
2023-03-06 12:58:10 -0300

    finalizando exercício INSS e IR

--- fundamentos/secao-3-introdjava/dia-1-primeirospassos/exercicio.js
+++ fundamentos/secao-3-introdjava/dia-1-primeirospassos/exercicio.js
@@ -102,3 +102,42 @@ else {
 } */
 
 
+
+let aliquotINSS;
+let aliquotIR;
+let grossSalary = 1000;
+
+if (grossSalary <= 1556.94) {
+    aliquotINSS = grossSalary * 0.08;
+}
+else if (grossSalary >= 1556.95 && grossSalary <= 2594.92) {
+    aliquotINSS = grossSalary * 0.09;
+}
+else if (grossSalary >= 2594.93 && grossSalary <= 5189.82) {
+    aliquotINSS = grossSalary * 0.11;
+}
+else {
+    aliquotINSS = 570.88;
+}
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+    aliquotIR = 0;
+}
+else if (baseSalary >= 1903.99 && baseSalary <= 2826.65) {
+    aliquotIR = baseSalary * 0.075 - 142.8;
+}
+else if (baseSalary >= 2826.66 && baseSalary <= 3751.05) {
+    aliquotIR = baseSalary * 0.15 - 354.8;
+}
+else if (baseSalary >= 3751.06 && baseSalary <= 4.664,68) {
+    aliquotIR = baseSalary * 0.225 - 636.13;
+}
+else {
+    aliquotIR = baseSalary * 0.275 - 869.36;
+}
+
+let finalSalary = baseSalary - aliquotIR;
+
+console.log('Salário: ' + finalSalary);
