cba177cffa7c6868a930d62f168c91bf4580ece9
Dandara Murad Cajaiba <dandymi@gmail.com>
2023-03-05 21:58:33 -0300

    exercício das peças de xadrez

--- fundamentos/secao-3-introdjava/dia-1-primeirospassos/exercicio.js
+++ fundamentos/secao-3-introdjava/dia-1-primeirospassos/exercicio.js
@@ -24,7 +24,7 @@ else {
     console.log('zero');
 } */
 
-let angle1 = 45;
+/* let angle1 = 45;
 let angle2 = 45;
 let angle3 = 90;
 let sumOfAngles = angle1 + angle2 + angle3;
@@ -39,4 +39,29 @@ else if (sumOfAngles !== 180) {
     else {
         console.log('false');
     }
+} */
+
+let chessPiece = 'Rainha';
+
+switch (chessPiece.toLowerCase()) {
+    case 'bispo':
+        console.log('diagonais');
+    break;
+    case 'rei':
+        console.log('uma casa em qualquer direção');
+    break;
+    case 'rainha':
+        console.log('diagonal, horizontal e vertical');
+    break;
+    case 'peão':
+        console.log('uma casa pra frente');
+    break;
+    case 'cavalo':
+        console.log('em formato de L');
+    break;
+    case 'torre':
+        console.log('horizontal e vertical');
+    break;
+    default:
+        console.log('peça inválida');
 }
