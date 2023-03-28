dbdace1f0790bdaa5391f996db43ee62fa7a96f0
Dandara Murad Cajaiba <dandymi@gmail.com>
2023-03-28 12:42:17 -0300

    myRemove teste 3 ok

--- dia-02-primeiros-passos-em-jest/script.test.js
+++ dia-02-primeiros-passos-em-jest/script.test.js
@@ -7,4 +7,7 @@ describe('Exercício 1', () => {
     it('Verifica se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
         expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
     });
+    it('Verifica se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
 });
